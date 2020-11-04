4654733457afc26419694ac5a8c4e02999aaf916
jfrench <joshua.french@gmail.com>
2020-11-04 11:41:02 -0700

    Reformat noc.

--- R/noc.R
+++ R/noc.R
@@ -1,25 +1,21 @@
 #' Determine non-overlapping clusters
-#' 
-#' Determine non-overlapping clusters from a list of
-#' potential clusters.
-#' 
-#' The function takes a list of potential clusters. Each
-#' element of the list contains a potential cluster. The
-#' potential clusters are defined by the location indices of
-#' the regions comprising the clusters.  Starting with the
-#' first potential cluster, the function excludes every
-#' potential cluster that intersects the first (any
-#' potential cluster that shares indices).  Moving onto the
-#' next non-overlapping cluster, the process is repeated. 
-#' The function returns the indices (in the list of
-#' clusters) of the clusters that do not overlap.
-#' 
+#'
+#' Determine non-overlapping clusters from a list of potential clusters.
+#'
+#' The function takes a list of potential clusters. Each element of the list
+#' contains a potential cluster. The potential clusters are defined by the
+#' location indices of the regions comprising the clusters.  Starting with the
+#' first potential cluster, the function excludes every potential cluster that
+#' intersects the first (any potential cluster that shares indices).  Moving
+#' onto the next non-overlapping cluster, the process is repeated. The function
+#' returns the indices (in the list of clusters) of the clusters that do not
+#' overlap.
+#'
 #' @param x A list containing the potential clusters.
-#' @return A vector with the list indices of the 
-#'   non-overlapping clusters.
+#' @return A vector with the list indices of the non-overlapping clusters.
 #' @author Joshua French
 #' @export
-#' @examples 
+#' @examples
 #' x = list(1:2, 1:3, 4:5, 4:6, 7:8)
 #' noc(x)
 noc = function(x) {
