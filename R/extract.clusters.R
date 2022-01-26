76b1b0cad6d57c75ca7e9df448b8c1a6e677a67e
jfrench <joshua.french@gmail.com>
2022-01-26 11:18:47 -0700

    Get clusters function working with import from smerc.

--- R/extract.clusters.R
+++ R/extract.clusters.R
@@ -11,10 +11,16 @@
 #' out = spscan.test(grave, nsim = 99)
 #' # print scan object
 #' clusters(out)
+#' @method clusters spscan
+#' @inheritParams smerc::clusters
 #' @export
 clusters.spscan = function(x, idx = seq_along(x$clusters), ...) {
   if (min(idx) < 1 | max(idx) > length(x$clusters)) {
     stop("invalid idx values")
   }
-  lget(x$clusters[idx], "locids")
+  smerc::lget(x$clusters[idx], "locids")
 }
+
+#' @importFrom smerc clusters
+#' @export
+smerc::clusters
